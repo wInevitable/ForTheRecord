019dc54cb052a67bde5dca8a4a2c6eed95d64f54
App Academy Student <student@appacademy.io>
2014-05-23 16:18:12 -0400

    Begin has one through.

--- lib/active_record_lite/05_associatable2.rb
+++ lib/active_record_lite/05_associatable2.rb
@@ -6,10 +6,23 @@ module Associatable
 
   def has_one_through(name, through_name, source_name)
     define_method(name) do
-      through_name =
-      assoc_options =
-      through_options =
+      through_options = self.class.assoc_options[through_name]
+
+      source_options = through_options.table_name
+
+      through_table = through_options.table_name
+      through_primary_key = through.primary_key
+      through_foreign_key = through.foreign_key
       
+      source_table = source_options.table_name
+      source_primary_key = source.primary_key
+      source_foreign_key = source.foreign_key
+      result = DBConnection.execute(<<-SQL)
+      
+      
+      SQL
+      
+      #parse results
     end
   end
 end
