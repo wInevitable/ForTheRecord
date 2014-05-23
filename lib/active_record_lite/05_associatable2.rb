4b74ef6798e1ded161e5f6159559c05f78030cc3
App Academy Student <student@appacademy.io>
2014-05-23 17:04:39 -0400

    Finish has one through. Project complete.

--- lib/active_record_lite/05_associatable2.rb
+++ lib/active_record_lite/05_associatable2.rb
@@ -8,21 +8,31 @@ module Associatable
     define_method(name) do
       through_options = self.class.assoc_options[through_name]
 
-      source_options = through_options.table_name
+      source_options = through_options.model_class.assoc_options[source_name]
 
       through_table = through_options.table_name
-      through_primary_key = through.primary_key
-      through_foreign_key = through.foreign_key
+      through_primary_key = through_options.primary_key
+      through_for_key = through_options.foreign_key
       
       source_table = source_options.table_name
-      source_primary_key = source.primary_key
-      source_foreign_key = source.foreign_key
-      result = DBConnection.execute(<<-SQL)
+      source_primary_key = source_options.primary_key
+      source_for_key = source_options.foreign_key
       
-      
+      value = self.send(through_for_key)
+      result = DBConnection.execute(<<-SQL, value)
+        SELECT
+          #{source_table}.*
+        FROM
+          #{through_table}
+        JOIN
+          #{source_table}
+        ON
+          #{through_table}.#{source_for_key} = #{source_table}.#{source_primary_key}
+        WHERE
+          #{through_table}.#{through_primary_key} = ?
       SQL
       
-      #parse results
+      source_options.model_class.parse_all(result).first
     end
   end
 end
